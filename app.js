2242229634c61d3e67784e20d7feaac1d7baefd4
Ghostwolf73 <solomzigumbo@gmail.com>
2022-07-27 18:08:14 +0200

    game done, all things done

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 let choices = ["rock", "paper", "scissors"];
+let i = 5;
 
-function getComputerChoice(arr){
+let computerWins = 0;
+let userWin = 0;
+
+function getComputerChoice(arr) {
     const randomIndex = Math.floor(Math.random() * arr.length);
 
     const item = arr[randomIndex];
@@ -8,49 +12,70 @@ function getComputerChoice(arr){
     return item;
 }
 
-let computerChoice = getComputerChoice(choices);
+function play(pc, user) {
+    let computerChoice = getComputerChoice(choices);
+    let choose = prompt("rock, paper or scissors?");
+    let playerChoice = choose.toLowerCase();
 
-
-let playerChoice = prompt("rock, paper or scissors?");
-
-function roundOne(pc, user){
     pc = computerChoice;
     user = playerChoice;
 
-    if (pc === "rock" && user === "paper"){
-        alert(`PC chose ${computerChoice} and you selected ${playerChoice} you win`);
+    if (pc === "rock" && user === "paper") {
+        console.log(`PC chose ${computerChoice} and you selected ${playerChoice} you win`);
+        userWin++
     }
-    else if (pc === "rock" && user === "scissors"){
-        alert(`PC chose ${computerChoice} and you selected ${playerChoice} you lose`);
+    else if (pc === "rock" && user === "scissors") {
+        console.log(`PC chose ${computerChoice} and you selected ${playerChoice} you lose`);
+        computerWins++
     }
-    else if (pc === "rock" && user === "rock"){
-        alert(`PC chose ${computerChoice} and you selected ${playerChoice} draw`);
-    }
-
-    else if (pc === "paper" && user === "scissors"){
-        alert(`PC chose ${computerChoice} and you selected ${playerChoice} you win`);
-    }
-    else if (pc === "paper" && user === "rock"){
-        alert(`PC chose ${computerChoice} and you selected ${playerChoice} you lose`);
+    else if (pc === "rock" && user === "rock") {
+        console.log(`PC chose ${computerChoice} and you selected ${playerChoice} draw`);
     }
 
-    else if (pc === "paper" && user === "paper"){
-        alert(`PC chose ${computerChoice} and you selected ${playerChoice} draw`);
+    else if (pc === "paper" && user === "scissors") {
+        console.log(`PC chose ${computerChoice} and you selected ${playerChoice} you win`);
+        userWin++
+    }
+    else if (pc === "paper" && user === "rock") {
+        console.log(`PC chose ${computerChoice} and you selected ${playerChoice} you lose`);
+        computerWins++
     }
 
-    else if (pc === "scissors" && user === "scissors"){
-        alert(`PC chose ${computerChoice} and you selected ${playerChoice} draw`);
-    }
-    else if (pc === "scissors" && user === "rock"){
-        alert(`PC chose ${computerChoice} and you selected ${playerChoice} you win`);
+    else if (pc === "paper" && user === "paper") {
+        console.log(`PC chose ${computerChoice} and you selected ${playerChoice} draw`);
     }
 
-    else if (pc === "scissors" && user === "paper"){
-        alert(`PC chose ${computerChoice} and you selected ${playerChoice} lose`);
+    else if (pc === "scissors" && user === "scissors") {
+        console.log(`PC chose ${computerChoice} and you selected ${playerChoice} draw`);
+    }
+    else if (pc === "scissors" && user === "rock") {
+        console.log(`PC chose ${computerChoice} and you selected ${playerChoice} you win`);
+        userWin++
+    }
+
+    else if (pc === "scissors" && user === "paper") {
+        console.log(`PC chose ${computerChoice} and you selected ${playerChoice} lose`);
+        computerWins++
     } else {
-        alert('invalid');
+        console.log('invalid');
     }
 
 }
 
-roundOne();
+function game() {
+    for (let i = 0; i < 5; i++) {
+
+        play();
+        console.log(`computer ${computerWins} and user ${userWin}`);
+    }
+}
+
+game()
+
+if (computerWins > userWin){
+    console.log("Computer wins");
+} else if (userWin > computerWins){
+    console.log("You win");
+} else (
+    console.log("was a draw, wanna try again?")
+)
